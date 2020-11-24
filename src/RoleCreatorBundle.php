6c17719b3370a70c1d923c785b0df4e7ad370901
Luke MacAusland <luke.macausland@torqit.ca>
2020-11-24 10:49:31 -0400

    Remove unused js path, to remove console error in admin

--- src/RoleCreatorBundle.php
+++ src/RoleCreatorBundle.php
@@ -6,10 +6,4 @@ use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 
 class RoleCreatorBundle extends AbstractPimcoreBundle
 {
-    public function getJsPaths()
-    {
-        return [
-            '/pimcorerolecreator/js/pimcore/startup.js'
-        ];
-    }
 }
