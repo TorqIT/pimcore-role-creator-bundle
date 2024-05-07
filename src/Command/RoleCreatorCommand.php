4e3431e9f65075a618722bb315e57f3e4aff4642
lukemacausland <58705994+lukemacausland@users.noreply.github.com>
2024-05-07 10:07:02 -0300

    Add perspective option.

--- src/Command/RoleCreatorCommand.php
+++ src/Command/RoleCreatorCommand.php
@@ -68,6 +68,7 @@ class RoleCreatorCommand extends AbstractCommand
         $this->applyPermissions($role, $roleProperties ?? []);
         $this->applyWorkspaces($role, $roleProperties ?? []);
         $this->applyAllowedTypes($role, $roleProperties ?? []);
+        $this->applyPerspectives($role, $roleProperties ?? []);
 
         $role->setParentId(0);
         $role->setName($roleName);
@@ -208,5 +209,15 @@ class RoleCreatorCommand extends AbstractCommand
         }
 
         return null;
+    }
+
+    private function applyPerspectives(Role $role, array $roleProperties): void
+    {
+        if (!key_exists("perspectives", $roleProperties)) {
+            $role->setPerspectives([]);
+            return;
+        }
+
+        $role->setPerspectives($roleProperties["perspectives"]);
     }
 }
